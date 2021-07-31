9042ff1f1db214e442c50214361ab0d7dcce5ed6
Radek Skrabucha <rskrabucha13@gmail.com>
2021-07-31 10:27:16 +0200

    added edit message and send image

--- src/App.js
+++ src/App.js
@@ -3,72 +3,124 @@ import { v4 as uuidv4 } from "uuid";
 import Moment from "react-moment";
 
 const USERS = {
-	John: {
-		id: 1,
-		firstName: "John",
-		lastName: "Doe",
+	john: {
+		id: "john",
+		name: "John Doe",
 		profilePic: "",
 		messages: [],
 	},
-	Krystian: {
-		id: 2,
-		firstName: "Krystian",
-		lastName: "Pach",
+	krystian: {
+		id: "krystian",
+		name: "Krystian Pach",
 		profilePic: "",
 		messages: [],
 	},
 };
 const App = () => {
 	const [users, setUsers] = useState(USERS);
-	const [currentUser, setCurrentUser] = useState(USERS.John);
+	const [currentUser, setCurrentUser] = useState(USERS.john);
 	const [messages, setMessages] = useState([]);
-	const [currentMessage, setCurrentMessage] = useState({ text: "", image: "" });
+	const [currentMessage, setCurrentMessage] = useState({
+		content: "",
+		image: "",
+		id: "",
+		time: 0,
+		user: {},
+	});
+	const [isEditing, setIsEditing] = useState(false);
 
 	const handleChange = (e) => {
 		const value = e.target.value;
-		const name = e.target.name;
-		setCurrentMessage({ ...currentMessage, [name]: value });
+		setCurrentMessage({ ...currentMessage, content: value });
+	};
+
+	const handleImageUpload = (e) => {
+		const [file] = e.target.files;
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				setCurrentMessage({ ...currentMessage, image: e.target.result });
+			};
+			reader.readAsDataURL(file);
+		}
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const newMessage = {
 			id: uuidv4(),
-			content: currentMessage.text,
+			content: currentMessage.content ? currentMessage.content : null,
+			image: currentMessage.image ? currentMessage.image : null,
 			time: Date.now(),
-			user: { currentUser },
+			user: { ...currentUser },
 		};
 		setUsers({
 			...users,
-			[currentUser.firstName]: {
-				...users[currentUser.firstName],
-				messages: [...users[currentUser.firstName].messages, { ...newMessage }],
+			[currentUser.id]: {
+				...users[currentUser.id],
+				messages: [...users[currentUser.id].messages, { ...newMessage }],
 			},
 		});
-		setCurrentMessage({ text: "", image: "" });
+		setCurrentMessage({
+			content: "",
+			image: "",
+			id: "",
+			time: 0,
+			user: {},
+		});
 	};
 
 	const handleSwitchUsers = (e) => {
 		const checked = e.target.checked;
-		checked ? setCurrentUser(USERS.Krystian) : setCurrentUser(USERS.John);
+		checked ? setCurrentUser(USERS.krystian) : setCurrentUser(USERS.john);
 	};
 
-	const deleteMessage = (id, firstName) => {
-		const newMessages = users[firstName].messages.filter(
-			(msg) => msg.id !== id
-		);
+	const deleteMessage = (id, userId) => {
+		const newMessages = users[userId].messages.filter((msg) => msg.id !== id);
 		setUsers({
 			...users,
-			[firstName]: {
-				...users[firstName],
+			[userId]: {
+				...users[userId],
 				messages: newMessages,
 			},
 		});
 	};
 
+	const editMessage = (msg) => {
+		setIsEditing(true);
+		setCurrentMessage(msg);
+	};
+
+	const handleEdit = (id, userId, e) => {
+		e.preventDefault();
+		const newContent = currentMessage.content;
+		const newImage = currentMessage.image;
+		const newMessages = users[userId].messages.map((msg) => {
+			if (msg.id === id) {
+				return { ...msg, content: newContent, image: newImage };
+			}
+			return msg;
+		});
+		setUsers({
+			...users,
+			[userId]: {
+				...users[userId],
+				messages: newMessages,
+			},
+		});
+		setCurrentMessage({
+			content: "",
+			image: "",
+			id: "",
+			time: 0,
+			user: {},
+		});
+		setIsEditing(false);
+	};
+
 	useEffect(() => {
 		setMessages(
-			[...users.John.messages, ...users.Krystian.messages].sort((a, b) => {
+			[...users.john.messages, ...users.krystian.messages].sort((a, b) => {
 				return a.time - b.time;
 			})
 		);
@@ -79,9 +131,7 @@ const App = () => {
 			<h1>Chat application</h1>
 			<div>
 				<h3>Switch Users</h3>
-				<p>
-					Current user: {`${currentUser.firstName} ${currentUser.lastName}`}
-				</p>
+				<p>Current user: {currentUser.name}</p>
 				<input onChange={handleSwitchUsers} type="checkbox" name="" id="" />
 			</div>
 			<section>
@@ -89,37 +139,69 @@ const App = () => {
 					{messages.map((msg) => {
 						const {
 							content,
+							image,
 							id,
 							time,
-							user: { firstName, lastName },
+							user: { name, id: userId },
 						} = msg;
 						return (
 							<div key={id}>
-								<p>{`${firstName} ${lastName}`}</p>
-								<h2>{content}</h2>
+								<p>{name}</p>
+								{content && <h2>{content}</h2>}
+								{image && (
+									<img
+										src={image}
+										alt=""
+										style={{
+											width: "100px",
+											height: "100px",
+											background: "red",
+										}}
+									/>
+								)}
 								<Moment format="k:mm">{time}</Moment>
-								<button
-									onClick={() => deleteMessage(id, firstName)}
-									type="button"
-								>
+								<button onClick={() => deleteMessage(id, userId)} type="button">
 									Delete
 								</button>
-								<button type="button">Edit</button>
+								<button onClick={() => editMessage(msg)} type="button">
+									Edit
+								</button>
 							</div>
 						);
 					})}
 				</div>
-				<form onSubmit={handleSubmit}>
+				<form
+					onSubmit={
+						isEditing
+							? (e) => handleEdit(currentMessage.id, currentUser.id, e)
+							: handleSubmit
+					}
+				>
 					<input
 						autoComplete="off"
-						value={currentMessage.text}
+						value={currentMessage.content}
 						onChange={handleChange}
 						type="text"
 						name="text"
 						id="text"
 					/>
-					<button type="submit">Send</button>
+					<input
+						onChange={handleImageUpload}
+						type="file"
+						accept="image/*"
+						multiple={false}
+					/>
+					<button type="submit">{isEditing ? "Save" : "Send"}</button>
 				</form>
+				<img
+					src={currentMessage.image}
+					alt=""
+					style={{
+						width: "100px",
+						height: "100px",
+						background: "red",
+					}}
+				/>
 			</section>
 		</div>
 	);
